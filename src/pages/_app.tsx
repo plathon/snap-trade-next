ecf653c6e6905922c3b221deb28d4172d931b38b
= <=>
2021-03-05 04:49:23 -0300

    feat: hero landing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,16 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
         href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
         rel="stylesheet"
       ></link>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+        integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+      />
     </Head>
     <Component {...pageProps} />
   </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,27 @@ const Home: FC = () => {
   return (
     <div>
       <div className={styles.topBar}>
-        <span>SNAP TRADE</span>
+        <span className={styles.title}>SNAP TRADE</span>
         <a className={styles.signInButton} href="#">
           Sign In
+        </a>
+      </div>
+      <div className={styles.hero}>
+        <div>
+          <h1>SNAP TRADE</h1>
+          <p>
+            Apply algorithms and professional metrics to evaluate
+            <br />
+            your strategies and validate your trade system
+          </p>
+          <div className={styles.actions}>
+            <a href="#">Sign Up Now!</a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.more}>
+        <a href="#">
+          <i className="fas fa-chevron-down animate__animated animate__bounce animate__repeat-3"></i>
         </a>
       </div>
     </div>
