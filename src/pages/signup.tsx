e44893e0542b3345ce61fe63a3be9316077bf6e3
= <=>
2021-03-06 21:31:39 -0300

    feat: signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react'
+import Link from 'next/link'
+
+import styles from './signup.module.css'
+
+const Signup: FC = () => {
+  return (
+    <>
+      <Link href="/">
+        <a className={styles.backIcon}>
+          <i className="far fa-arrow-alt-circle-left"></i>
+        </a>
+      </Link>
+      <div className={styles.signUpPage}>
+        <div>
+          <h1>Sign Up Now for free! ;)</h1>
+        </div>
+        <div>
+          <form>
+            <div>
+              <p>Name</p>
+              <input type="text" name="name" />
+            </div>
+            <div>
+              <p>Email</p>
+              <input type="email" name="email" />
+            </div>
+            <div>
+              <p>Password</p>
+              <input type="password" name="password" />
+            </div>
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Signup
